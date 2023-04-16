99ccc864b0aa1413c53a466b02600602cae38c14
KvDmtr <dimaskulich@gmail.com>
2023-04-17 02:17:03 +0300

    brought out the general logic

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import readlineSync from 'readline-sync';
+import greetings from './cli.js';
+
+// случайное число в диапазоне от 0 до 100(включительно)
+export function getNumber() {
+  const min = 1;
+  const max = 100;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let count = 1;
+const userName = greetings();
+
+export function getGame(nameGame, rule, question, answer) {
+  if (count === 1) {
+    console.log(rule);
+  }
+  if (count === 4) {
+    return console.log(`Congratulations, ${userName}!`);
+  }
+  console.log(question);
+  //   console.log(answer);
+  const userAnswer = readlineSync.question('Your answer: ');
+  if (!(userAnswer === answer)) {
+    const error = `"${userAnswer}" is wrong answer ;(. Correct answer was "${answer}".
+Let's try again, ${userName}!`;
+    return console.log(error);
+  }
+  count += 1;
+  console.log('Correct!');
+  return nameGame();
+}

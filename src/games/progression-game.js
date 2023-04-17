c97d1c5eb019fd3e39be9b491c0cff6502d4d060
KvDmtr <dimaskulich@gmail.com>
2023-04-18 00:43:56 +0300

    add brain-progression game logic

---
+++ src/games/progression-game.js
@@ -0,0 +1,48 @@
+import { getGame, getNumber } from '../index.js';
+
+// Выбирается случайный шаг прогрессии от 1 до 10(включительно)
+function randomStep() {
+  const progressionStep = Math.floor(Math.random() * 10 + 1);
+  return progressionStep;
+}
+
+function getProgressionLength() {
+  const minLength = 5;
+  const maxLength = 10;
+  const progressionLength = minLength + Math.random() * (maxLength + 1 - minLength);
+  return Math.floor(progressionLength);
+}
+
+function getProgression() {
+  const progression = [];
+  const progressionLength = getProgressionLength();
+  const progressStep = randomStep();
+  const startValue = Math.floor(getNumber() / 2);
+  for (let number = startValue; progression.length < progressionLength; number += progressStep) {
+    progression.push(number);
+  }
+  const hideRandom = Math.floor(Math.random() * progression.length);
+  progression[hideRandom] = '..';
+  const question = progression.join(' ');
+  return [question, progressStep];
+}
+
+function getAnswer(question, progressStep) {
+  const progression = question.split(' ');
+  const hideNumber = progression.indexOf('..');
+  let previousNum = 0;
+  if (hideNumber === 0) {
+    previousNum = +progression[hideNumber + 1];
+    return `${previousNum - progressStep}`;
+  }
+  previousNum = +progression[hideNumber - 1];
+  return `${previousNum + progressStep}`;
+}
+
+export default function progressionGame() {
+  const nameGame = progressionGame;
+  const rule = 'What number is missing in the progression?';
+  const [question, progressStep] = getProgression();
+  const answer = getAnswer(question, progressStep);
+  getGame(nameGame, rule, question, answer);
+}
